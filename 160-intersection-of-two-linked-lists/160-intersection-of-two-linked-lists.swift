2d4a9eb19d3c33dc3c3cc9e0fef44078b9495b89
ohdair <79438622+ohdair@users.noreply.github.com>
2022-10-01 17:59:21 +0900

    Time: 224 ms (99.52%), Space: 17.1 MB (19.62%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.swift
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.swift
@@ -12,45 +12,16 @@
 
 class Solution {
     func getIntersectionNode(_ headA: ListNode?, _ headB: ListNode?) -> ListNode? {
-        guard headA != nil, headB != nil else { return nil }
-        
-        let countA: Int = listCount(headA)
-        let countB: Int = listCount(headB)
-        var different: Int = abs(countA - countB)
+        if headA == nil || headB == nil { return nil }
         
         var a: ListNode? = headA
         var b: ListNode? = headB
-        var c: ListNode?
-        
-        if countA > countB {
-            while different > 0 {
-                a = a?.next
-                different -= 1
-            }
-        } else if countA < countB {
-            while different > 0 {
-                b = b?.next
-                different -= 1
-            }
-        }
-        
         
         while a !== b {
-            a = a?.next
-            b = b?.next
+            a = a == nil ? headB : a?.next
+            b = b == nil ? headA : b?.next
         }
         
-        return a
-    }
-    
-    func listCount(_ head: ListNode?) -> Int {
-        var count: Int = 0
-        var head = head 
-        
-        while head != nil {
-            head = head?.next
-            count += 1
-        }
-        return count
+        return a;
     }
 }
