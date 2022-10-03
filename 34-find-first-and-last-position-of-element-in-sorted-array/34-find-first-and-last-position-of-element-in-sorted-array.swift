233a6b78abfcd85f0de0717c989ecd3b8571bf75
ohdair <79438622+ohdair@users.noreply.github.com>
2022-10-03 11:40:53 +0900

    Time: 51 ms (75.00%), Space: 15.3 MB (46.79%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.swift
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.swift
@@ -1,16 +1,13 @@
 class Solution {
     func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
-        var find: Bool = false
         var result: [Int] = [-1, -1]
         
         for (index, num) in nums.enumerated() {
             if num == target {
-                if !find {
-                    result[0] = index
-                    result[1] = index
-                    find = true
+                if result[0] == -1 {
+                    result = [index, index]
                 } else {
-                    result[1] = index    
+                    result[1] = index
                 }
             }
         }
