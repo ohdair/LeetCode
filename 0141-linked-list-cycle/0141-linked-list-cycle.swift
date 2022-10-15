b45964531cc98287821db13b05a9c864d0ece8f4
ohdair <79438622+ohdair@users.noreply.github.com>
2022-10-15 12:40:44 +0900

    Time: 57 ms (93.19%), Space: 15.5 MB (90.92%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.swift
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+import Foundation
+
+class Solution {
+    func hasCycle(_ head: ListNode?) -> Bool {
+        var slow = head
+        var fast = head?.next
+        
+        while slow != nil, fast != nil {
+            guard slow !== fast else { return true }
+            
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+        
+        return false
+    }
+}
