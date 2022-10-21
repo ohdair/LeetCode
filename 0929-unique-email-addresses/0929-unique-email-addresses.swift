aad126781cbc928fd0f22edf237470c4f09f50a4
ohdair <79438622+ohdair@users.noreply.github.com>
2022-10-21 16:53:59 +0900

    Time: 192 ms (73.02%), Space: 14.6 MB (87.30%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.swift
+++ 0929-unique-email-addresses/0929-unique-email-addresses.swift
@@ -1,26 +1,18 @@
 class Solution {
     func numUniqueEmails(_ emails: [String]) -> Int {
         var uniqueEmails: Set<String> = []
+        
         for email in emails {
-            uniqueEmails.insert(transEmail(email))
+            var valid = email.split(separator: "@")
+            var name = String()
+            
+            for ch in valid[0] where ch != "." {
+                guard ch != "+" else { break }
+                name.append(ch)
+            }
+            uniqueEmails.insert(name + "@" + valid[1])
         }
         
         return uniqueEmails.count
-    }
-    
-    func transEmail(_ str: String) -> String {
-        var email = Array(str)
-        
-        var idx = Int(email.firstIndex(of: "@")!)
-        var domain: String = String(email[idx..<email.count])
-        email.removeSubrange(idx..<email.count)
-        
-        if let idx = email.firstIndex(of: "+") {
-            email.removeSubrange(Int(idx)..<email.count)
-        }
-        
-        var ret = email.filter { $0 != "." }
-        
-        return String(ret) + domain
     }
 }
