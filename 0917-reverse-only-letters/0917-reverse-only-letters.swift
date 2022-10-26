b552ab454044110e323ca69cbd199695b9fcd511
ohdair <79438622+ohdair@users.noreply.github.com>
2022-10-26 10:31:51 +0900

    Time: 4 ms (82.14%), Space: 14.5 MB (25.00%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.swift
+++ 0917-reverse-only-letters/0917-reverse-only-letters.swift
@@ -1,21 +1,13 @@
 class Solution {
     func reverseOnlyLetters(_ s: String) -> String {
-        var start = 0
-        var end = s.count - 1
+        var arr = Array(s)
+        var letter = arr.filter { $0.isLetter }
+        var ret: [Character] = []
         
-        var s = Array(s)
-        
-        while start < end {
-            while s[start].isLetter == false, end > start { start += 1 }
-            while s[end].isLetter == false, end > 0 { end -= 1 }
-            guard start < end else { break }
-            let temp = s[start]
-            s[start] = s[end]
-            s[end] = temp
-            start += 1
-            end -= 1
+        for c in arr {
+            ret.append(c.isLetter ? letter.removeLast() : c)
         }
         
-        return String(s)
+        return String(ret)
     }
 }
